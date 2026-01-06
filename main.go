4c02592fb41def73b70ed533ead3696b23750f20
Jamleck <tumkibet32@gmail.com>
2026-01-06 18:19:02 +0300

    Refactor ticket variables to use uint type and improve user input handling

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 
 	conferenceName := "Go  confrence"
-	const conferenceTicket int = 50
+	const conferenceTicket uint = 50
 
-	var remainingTickets int = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("confrenceTicket is %T,  remaining tickets is %T,  confrencece Name  is %T\n", conferenceTicket, remainingTickets, conferenceName)
 
@@ -15,12 +15,37 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available. \n", conferenceTicket, remainingTickets)
 	fmt.Println("Get your ticket  here to attend ")
 
-	var userName string
-	var userTickets int
+	var bookings [50]string
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
 	// ask user for their name
+	fmt.Println("Enter your first name ?  ")
+	fmt.Scan(&firstName)
 
-	userName = "Tom"
-	userTickets = 2
+	fmt.Println("Enter your last name ? ")
+	fmt.Scan(&lastName)
 
-	fmt.Printf("User %v booked  %v tickets. \n", userName, userTickets)
+	fmt.Println("Enter your  email ? ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets ? ")
+	fmt.Scan(&userTickets)
+
+	// updating the remainig tickets
+	remainingTickets = conferenceTicket - userTickets
+
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The  whole array : %v \n", bookings)
+	fmt.Printf("The first valu: %v \n", bookings[0])
+	fmt.Printf("the type: %T \n", bookings)
+	fmt.Printf("Array lenght: %v \n", len(bookings))
+
+	fmt.Printf("Thank you  for booking ticket with us  \n first name  %v  \n last name	%v  \n email %v \n with  booked  %v tickets. \n", firstName, lastName, email, userTickets)
+
+	fmt.Println("We have ", remainingTickets, "tickets remaining ")
 }
